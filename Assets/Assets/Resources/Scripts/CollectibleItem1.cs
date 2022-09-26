28fe1722b2c1c3c95a93b50821e891e3eade3d61
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-09-27 01:09:21 +0300

    Update

---
+++ Assets/Assets/Resources/Scripts/CollectibleItem1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleItem1 : MonoBehaviour {
+	[SerializeField] private string itemName;
+
+         void OnTriggerEnter(Collider other) {
+		Managers.Inventory.AddItem(itemName);
+		Destroy(this.gameObject);
+	     } 
+    
+  
+}
